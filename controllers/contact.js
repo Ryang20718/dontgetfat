799b5d0e411081e9e1c8e39c44df1ac634f6d628
Ryan Yang <Ryanyang99@hotmail.com>
2018-08-21 13:45:58 -0700

    update

---
+++ controllers/contact.js
@@ -0,0 +1,102 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'SendGrid',
+  auth: {
+    user: process.env.SENDGRID_USER,
+    pass: process.env.SENDGRID_PASSWORD
+  }
+});
+
+/**
+ * GET /contact
+ * Contact form page.
+ */
+exports.getContact = (req, res) => {
+  const unknownUser = !(req.user);
+
+  res.render('contact', {
+    title: 'Contact',
+    unknownUser,
+  });
+};
+
+/**
+ * POST /contact
+ * Send a contact form via Nodemailer.
+ */
+exports.postContact = (req, res) => {
+  let fromName;
+  let fromEmail;
+  if (!req.user) {
+    req.assert('name', 'Name cannot be blank').notEmpty();
+    req.assert('email', 'Email is not valid').isEmail();
+  }
+  req.assert('message', 'Message cannot be blank').notEmpty();
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/contact');
+  }
+
+  if (!req.user) {
+    fromName = req.body.name;
+    fromEmail = req.body.email;
+  } else {
+    fromName = req.user.profile.name || '';
+    fromEmail = req.user.email;
+  }
+    console.log(fromEmail);
+getReceiver(fromName,fromEmail,req.body.message);
+  const mailOptions = {
+    to: 'ryanyang99@hotmail.com',
+    from: `${fromName} <${fromEmail}>`,
+    subject: 'Inquiries From Website',
+    text: req.body.message
+  };
+
+  transporter.sendMail(mailOptions, (err) => {
+    if (err) {
+      req.flash('errors', { msg: err.message });
+      return res.redirect('/contact');
+    }
+    req.flash('success', { msg: 'Email has been sent successfully!' });
+    res.redirect('/contact');
+  });
+};
+
+
+function getReceiver(name,email,message){
+    
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
+
+var mailOptions = {
+  from: email,
+  to: 'ryanyang99@hotmail.com',
+  subject: "From " + email + " Website inquiries",
+  text: message
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+}
+
+exports.newCustomer = function(newName) {
+var newEmail = 'New Website Visitor Email Who viewed your resume is '+ newName;
+getReceiver('new Customer From Website Signed Up','ryanzonson@gmail.com',newEmail)
+};
+
